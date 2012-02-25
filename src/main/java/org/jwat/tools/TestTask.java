bea20e2059e8042ae3c363226bb6c0fb5df9f438
Nicholas Clarke <nicl@kb.dk>
2012-02-25 13:19:31 +0100

    Changed to use the new unified error/warning classes in jwat. Started on compression task but changes required to arc/warc packages before compression is possible.

--- src/main/java/org/jwat/tools/TestTask.java
+++ src/main/java/org/jwat/tools/TestTask.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
@@ -13,7 +12,6 @@ import org.jwat.arc.ArcReader;
 import org.jwat.arc.ArcReaderFactory;
 import org.jwat.arc.ArcRecord;
 import org.jwat.arc.ArcRecordBase;
-import org.jwat.arc.ArcValidationError;
 import org.jwat.arc.ArcVersionBlock;
 import org.jwat.common.ByteCountingPushBackInputStream;
 import org.jwat.common.Diagnosis;
@@ -22,7 +20,6 @@ import org.jwat.gzip.GzipReader;
 import org.jwat.warc.WarcReader;
 import org.jwat.warc.WarcReaderFactory;
 import org.jwat.warc.WarcRecord;
-import org.jwat.warc.WarcValidationError;
 
 public class TestTask extends Task {
 
@@ -59,10 +56,14 @@ public class TestTask extends Task {
 		ArcReader arcReader = null;
 		WarcReader warcReader = null;
 		int gzipEntries = 0;
+		int gzipErrors = 0;
+		int gzipWarnings = 0;
 		int arcRecords = 0;
 		int arcErrors = 0;
+		int arcWarnings = 0;
 		int warcRecords = 0;
 		int warcErrors = 0;
+		int warcWarnings = 0;
 		try {
 			pbin = new ByteCountingPushBackInputStream( new BufferedInputStream( new FileInputStream( srcFile ), 8192 ), 16 );
 			if ( GzipReader.isGzipped( pbin ) ) {
@@ -84,9 +85,8 @@ public class TestTask extends Task {
 							if ( version != null ) {
 							    ++arcRecords;
 							    version.close();
-								if (version.hasErrors()) {
-									arcErrors += version.getValidationErrors().size();
-								}
+								arcErrors += version.diagnostics.getErrors().size();
+								arcWarnings += version.diagnostics.getWarnings().size();
 								if ( bShowErrors ) {
 									showArcErrors( srcFile, version );
 								}
@@ -111,9 +111,8 @@ public class TestTask extends Task {
 								if ( arcRecord != null ) {
 								    ++arcRecords;
 								    arcRecord.close();
-									if (arcRecord.hasErrors()) {
-										arcErrors += arcRecord.getValidationErrors().size();
-									}
+									arcErrors += arcRecord.diagnostics.getErrors().size();
+									arcWarnings += arcRecord.diagnostics.getWarnings().size();
 									if ( bShowErrors ) {
 										showArcErrors( srcFile, arcRecord );
 									}
@@ -129,9 +128,8 @@ public class TestTask extends Task {
 						while ( (warcRecord = warcReader.getNextRecordFrom( in ) ) != null ) {
 							++warcRecords;
 							warcRecord.close();
-							if (warcRecord.hasErrors()) {
-								warcErrors += warcRecord.getValidationErrors().size();
-							}
+							warcErrors += warcRecord.diagnostics.getErrors().size();
+							warcWarnings += warcRecord.diagnostics.getWarnings().size();
 							if ( bShowErrors ) {
 								showWarcErrors( srcFile, warcRecord );
 							}
@@ -143,7 +141,11 @@ public class TestTask extends Task {
 					}
 					in.close();
 					gzipEntry.close();
-					showGzipErrors(srcFile, gzipEntry);
+					gzipErrors = gzipEntry.diagnostics.getErrors().size();
+					gzipWarnings = gzipEntry.diagnostics.getWarnings().size();
+					if ( bShowErrors ) {
+						showGzipErrors(srcFile, gzipEntry);
+					}
 					offset = pbin.getConsumed();
 				}
 				if ( arcReader != null ) {
@@ -167,9 +169,8 @@ public class TestTask extends Task {
 						if ( arcRecord != null ) {
 						    ++arcRecords;
 						    arcRecord.close();
-							if (arcRecord.hasErrors()) {
-								arcErrors += arcRecord.getValidationErrors().size();
-							}
+							arcErrors += arcRecord.diagnostics.getErrors().size();
+							arcWarnings += arcRecord.diagnostics.getWarnings().size();
 							if ( bShowErrors ) {
 								showArcErrors( srcFile, arcRecord );
 							}
@@ -190,9 +191,8 @@ public class TestTask extends Task {
 				while ( (warcRecord = warcReader.getNextRecord()) != null ) {
 					++warcRecords;
 					warcRecord.close();
-					if (warcRecord.hasErrors()) {
-						warcErrors += warcRecord.getValidationErrors().size();
-					}
+					warcErrors += warcRecord.diagnostics.getErrors().size();
+					warcWarnings += warcRecord.diagnostics.getWarnings().size();
 					if ( bShowErrors ) {
 						showWarcErrors( srcFile, warcRecord );
 					}
@@ -223,16 +223,20 @@ public class TestTask extends Task {
 			System.out.println( "Summary of '" + srcFile.getPath() + "'" );
 			if ( gzipEntries > 0 ) {
 				System.out.println( "    GZip.Entries: " + gzipEntries );
+				System.out.println( "     GZip.Errors: " + gzipErrors );
+				System.out.println( "   GZip.Warnings: " + gzipWarnings );
 			}
 			if ( arcReader != null ) {
 				System.out.println( "     Arc.isValid: " + arcReader.isCompliant() );
 				System.out.println( "     Arc.Records: " + arcRecords );
 				System.out.println( "      Arc.Errors: " + arcErrors );
+				System.out.println( "    Arc.Warnings: " + arcWarnings );
 			}
 			if ( warcReader != null ) {
 				System.out.println( "    Warc.isValid: " + warcReader.isCompliant() );
 				System.out.println( "    Warc.Records: " + warcRecords );
 				System.out.println( "     Warc.Errors: " + warcErrors );
+				System.out.println( "   Warc.Warnings: " + warcWarnings );
 			}
 		}
 	}
@@ -256,51 +260,29 @@ public class TestTask extends Task {
 		}
 	}
 
-	protected void showDiagnosisList(Iterator<Diagnosis> diagnosisIterator) {
-		Diagnosis diagnosis;
-		while (diagnosisIterator.hasNext()) {
-			diagnosis = diagnosisIterator.next();
-			System.out.println( "         Type: " + diagnosis.type.name() );
-			System.out.println( "       Entity: " + diagnosis.entity );
-			switch (diagnosis.type) {
-			case EMPTY:
-				break;
-			case INVALID:
-			case RESERVED:
-			case UNKNOWN:
-				System.out.println( "        Value: " + diagnosis.information[0] );
-				break;
-			case INVALID_EXPECTED:
-				System.out.println( "        Value: " + diagnosis.information[0] );
-				System.out.println( "     Expected: " + diagnosis.information[1] );
-				break;
-			case INVALID_ENCODING:
-				System.out.println( "        Value: " + diagnosis.information[0] );
-				System.out.println( "     Encoding: " + diagnosis.information[1] );
-				break;
-			}
-		}
-	}
-
 	protected void showArcErrors(File file, ArcRecordBase arcRecord) {
-		Collection<ArcValidationError> arcValidationErrors = arcRecord.getValidationErrors();
-		if ( arcValidationErrors != null ) {
+		List<Diagnosis> diagnosisList;
+		Iterator<Diagnosis> diagnosisIterator;
+		if ( arcRecord.diagnostics.hasErrors() ) {
 			System.out.println( "Error in '" + file.getPath() + "'" );
 			System.out.println( "       Offset: " + arcRecord.getStartOffset() + " (0x" + (Long.toHexString(arcRecord.getStartOffset())) + ")" );
-			Iterator<ArcValidationError> iter = arcValidationErrors.iterator();
-			ArcValidationError arcError;
-			while ( iter.hasNext() ) {
-				arcError = iter.next();
-				System.out.println( "   Error type: " + arcError.error );
-				System.out.println( "   Field/Desc: " + arcError.field );
-				System.out.println( "        Value: " + arcError.value );
-			}
+			diagnosisList = arcRecord.diagnostics.getErrors();
+			diagnosisIterator = diagnosisList.iterator();
+			showDiagnosisList(diagnosisIterator);
+		}
+		if ( arcRecord.diagnostics.hasWarnings() ) {
+			System.out.println( "Error in '" + file.getPath() + "'" );
+			System.out.println( "       Offset: " + arcRecord.getStartOffset() + " (0x" + (Long.toHexString(arcRecord.getStartOffset())) + ")" );
+			diagnosisList = arcRecord.diagnostics.getWarnings();
+			diagnosisIterator = diagnosisList.iterator();
+			showDiagnosisList(diagnosisIterator);
 		}
 	}
 
 	protected void showWarcErrors(File file, WarcRecord warcRecord) {
-		Collection<WarcValidationError> warcValidationErrors = warcRecord.getValidationErrors();
-		if ( warcValidationErrors != null ) {
+		List<Diagnosis> diagnosisList;
+		Iterator<Diagnosis> diagnosisIterator;
+		if ( warcRecord.diagnostics.hasErrors() ) {
 			String warcTypeStr = warcRecord.warcTypeStr;
 			if ( warcTypeStr == null || warcTypeStr.length() == 0 ) {
 				warcTypeStr = "unknown";
@@ -311,13 +293,69 @@ public class TestTask extends Task {
 			System.out.println( "Error in '" + file.getPath() + "'" );
 			System.out.println( "       Offset: " + warcRecord.getStartOffset() + " (0x" + (Long.toHexString(warcRecord.getStartOffset())) + ")" );
 			System.out.println( "  Record Type: " + warcTypeStr );
-			Iterator<WarcValidationError> iter = warcValidationErrors.iterator();
-			WarcValidationError warcError;
-			while ( iter.hasNext() ) {
-				warcError = iter.next();
-				System.out.println( "   Error type: " + warcError.error );
-				System.out.println( "   Field/Desc: " + warcError.field );
-				System.out.println( "        Value: " + warcError.value );
+			diagnosisList = warcRecord.diagnostics.getErrors();
+			diagnosisIterator = diagnosisList.iterator();
+			showDiagnosisList(diagnosisIterator);
+		}
+		if ( warcRecord.diagnostics.hasWarnings() ) {
+			String warcTypeStr = warcRecord.warcTypeStr;
+			if ( warcTypeStr == null || warcTypeStr.length() == 0 ) {
+				warcTypeStr = "unknown";
+			}
+			else {
+				warcTypeStr = "'" + warcTypeStr + "'";
+			}
+			System.out.println( "Warning in '" + file.getPath() + "'" );
+			System.out.println( "       Offset: " + warcRecord.getStartOffset() + " (0x" + (Long.toHexString(warcRecord.getStartOffset())) + ")" );
+			System.out.println( "  Record Type: " + warcTypeStr );
+			diagnosisList = warcRecord.diagnostics.getWarnings();
+			diagnosisIterator = diagnosisList.iterator();
+			showDiagnosisList(diagnosisIterator);
+		}
+	}
+
+	protected void showDiagnosisList(Iterator<Diagnosis> diagnosisIterator) {
+		Diagnosis diagnosis;
+		while (diagnosisIterator.hasNext()) {
+			diagnosis = diagnosisIterator.next();
+			System.out.println( "         Type: " + diagnosis.type.name() );
+			System.out.println( "       Entity: " + diagnosis.entity );
+			switch (diagnosis.type) {
+			case EMPTY:
+			case INVALID:
+				break;
+			case RECOMMENDED:
+				if (diagnosis.information != null) {
+					if (diagnosis.information.length >= 1) {
+						System.out.println( "  Recommended: " + diagnosis.information[0] );
+					}
+					if (diagnosis.information.length >= 2) {
+						System.out.println( "   Instead of: " + diagnosis.information[1] );
+					}
+				}
+				break;
+			case REQUIRED_INVALID:
+			case UNDESIRED_DATA:
+				if (diagnosis.information != null) {
+					if (diagnosis.information.length >= 1) {
+						System.out.println( "        Value: " + diagnosis.information[0] );
+					}
+				}
+				break;
+			case DUPLICATE:
+			case RESERVED:
+			case UNKNOWN:
+			case INVALID_DATA:
+				System.out.println( "        Value: " + diagnosis.information[0] );
+				break;
+			case INVALID_EXPECTED:
+				System.out.println( "        Value: " + diagnosis.information[0] );
+				System.out.println( "     Expected: " + diagnosis.information[1] );
+				break;
+			case INVALID_ENCODING:
+				System.out.println( "        Value: " + diagnosis.information[0] );
+				System.out.println( "     Encoding: " + diagnosis.information[1] );
+				break;
 			}
 		}
 	}
